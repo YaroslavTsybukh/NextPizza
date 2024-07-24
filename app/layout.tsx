83c7dd5064fe0e5b3efcbde4393301813b599f98
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2024-07-24 10:55:20 +0300

    markup header section is ready

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from 'next';
 import { Nunito } from 'next/font/google';
+import { Header } from '@/components/shared';
+
 import './globals.css';
 
 const nunito = Nunito({
@@ -21,6 +23,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={nunito.className}>
+                <Header />
                 <main className="min-h-screen">{children}</main>
             </body>
         </html>
---
+++ components/shared/header.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import { Container } from './container';
+import { Button } from '../ui/button';
+import Image from 'next/image';
+import { ArrowRight, ShoppingCart, User } from 'lucide-react';
+
+export const Header: FC = () => {
+    return (
+        <header className="my-10">
+            <Container className="flex items-center justify-between">
+                <div className="flex items-center gap-4">
+                    <Image src="/logo.png" width={35} height={35} alt="Logo" />
+                    <div>
+                        <p className="text-2xl font-black uppercase">
+                            NEXT PIZZA
+                        </p>
+                        <p className="text-base text-gray-400">
+                            вкусней уже некуда
+                        </p>
+                    </div>
+                </div>
+                <div className="flex items-center gap-3">
+                    <Button variant="outline" className="gap-1">
+                        <User size={16} />
+                        Войти
+                    </Button>
+
+                    <Button className="group relative">
+                        <b>520 ₽</b>
+                        <span className="mx-3 h-full w-[1px] bg-white/30" />
+                        <div className="flex items-center gap-1 transition duration-300 group-hover:opacity-0">
+                            <ShoppingCart size={16} />
+                            <b>3</b>
+                        </div>
+                        <ArrowRight className="absolute right-5 w-5 -translate-x-2 opacity-0 transition duration-300 group-hover:translate-x-0 group-hover:opacity-100" />
+                    </Button>
+                </div>
+            </Container>
+        </header>
+    );
+};
