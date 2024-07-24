c581c90967479ce3cbe7322794b7c3bb988b0d55
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2024-07-24 06:49:44 +0300

    init project

---
+++ prettier.config.mjs
@@ -0,0 +1,11 @@
+const config = {
+  tabWidth: 4,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSameLine: false,
+  bracketSpacing: true,
+  plugins: ["./node_modules/prettier-plugin-tailwindcss/dist/index.mjs"],
+};
+
+export default config;
