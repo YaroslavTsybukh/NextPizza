caf144625ba44271d031f42aba8dc6773fc0bfb9
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2025-03-27 21:23:47 +0200

    work with removeItem function and fix errors

--- shared/components/shared/cart-drawer.tsx
+++ shared/components/shared/cart-drawer.tsx
@@ -2,13 +2,16 @@
 
 import { FC, PropsWithChildren, useEffect } from 'react';
 import Link from 'next/link';
-import { ArrowRight } from 'lucide-react';
+import Image from 'next/image';
+import { ArrowLeft, ArrowRight } from 'lucide-react';
 import { useShallow } from 'zustand/react/shallow';
-import { Sheet, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/shared/components/ui/sheet';
+
+import { Sheet, SheetClose, SheetContent, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from '@/shared/components/ui/sheet';
+import { Button } from '@/shared/components/ui/button';
 import { useCartStore } from '@/shared/store';
 import { getCartItemDetails } from '@/shared/lib';
-import { Button } from '../ui/button';
-import { CartDrawerItem } from '.';
+import { cn } from '@/shared/lib/utils';
+import { CartDrawerItem, Title } from '.';
 import { PizzaSize, PizzaType } from '@/shared/constants/pizza';
 
 interface IProps {
@@ -34,49 +37,77 @@ export const CartDrawer: FC<PropsWithChildren<IProps>> = ({ children, className 
             <SheetTrigger asChild>{children}</SheetTrigger>
 
             <SheetContent className="flex flex-col justify-between bg-[#F4F1EE] pb-0">
-                <SheetHeader>
-                    <SheetTitle>
-                        В корзине <span className="font-bold">{items.length} товара</span>
-                    </SheetTitle>
-                </SheetHeader>
+                <div className={cn('flex h-full flex-col', !totalAmount && 'justify-center')}>
+                    {totalAmount > 0 && (
+                        <SheetHeader>
+                            <SheetTitle>
+                                В корзине <span className="font-bold">{items.length} товара</span>
+                            </SheetTitle>
+                        </SheetHeader>
+                    )}
 
-                {/* Items */}
-                <div className="scrollbar -mx-6 mt-5 flex-1 overflow-auto">
-                    {items.map((item) => (
-                        <div className="mb-2" key={item.id}>
-                            <CartDrawerItem
-                                id={item.id}
-                                imageUrl={item.imageUrl}
-                                details={getCartItemDetails(item.pizzaType as PizzaType, item.pizzaSize as PizzaSize, item.ingredients)}
-                                name={item.name}
-                                price={item.price}
-                                quantity={item.quantity}
-                                onClickCountButton={(type) => onClickCountButton(item.id, item.quantity, type)}
-                                onClickRemove={() => removeCartItem(item.id)}
-                            />
+                    {!totalAmount && (
+                        <div className="mx-auto flex w-72 flex-col items-center justify-center">
+                            <Image src="/assets/images/empty-box.png" alt="Empty cart" width={120} height={120} />
+                            {/* <Title size="sm" text="Корзина пустая" className="my-2 text-center font-bold" /> */}
+
+                            {
+                                //TODO: добавить стилей для тайтла.
+                            }
+                            <SheetTitle>Корзина пустая</SheetTitle>
+                            <p className="mb-5 text-center text-neutral-500">Добавьте хотя бы одну пиццу, чтобы совершить заказ</p>
+                            <SheetClose asChild>
+                                <Button className="h-12 w-56 text-base" size="lg">
+                                    <ArrowLeft className="mr-2 w-5" />
+                                    Вернуться назад
+                                </Button>
+                            </SheetClose>
                         </div>
-                    ))}
+                    )}
+
+                    {totalAmount > 0 && (
+                        <>
+                            {/* Items */}
+                            <div className="scrollbar -mx-6 mt-5 flex-1 overflow-auto">
+                                {items.map((item) => (
+                                    <div className="mb-2" key={item.id}>
+                                        <CartDrawerItem
+                                            id={item.id}
+                                            imageUrl={item.imageUrl}
+                                            details={getCartItemDetails(item.pizzaType as PizzaType, item.pizzaSize as PizzaSize, item.ingredients)}
+                                            name={item.name}
+                                            price={item.price}
+                                            quantity={item.quantity}
+                                            disabled={item.disabled}
+                                            onClickCountButton={(type) => onClickCountButton(item.id, item.quantity, type)}
+                                            onClickRemove={() => removeCartItem(item.id)}
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+
+                            <SheetFooter className="-mx-6 bg-white p-8">
+                                <div className="w-full">
+                                    <div className="mb-4 flex">
+                                        <span className="flex flex-1 text-lg text-neutral-500">
+                                            Итого
+                                            <div className="relative -top-1 mx-2 flex-1 border-b border-dashed border-b-neutral-200" />
+                                        </span>
+
+                                        <span className="text-lg font-bold">{totalAmount} грн</span>
+                                    </div>
+
+                                    <Link href="/cart">
+                                        <Button type="submit" className="h-12 w-full text-base">
+                                            Оформить заказ
+                                            <ArrowRight className="ml-2 w-5" />
+                                        </Button>
+                                    </Link>
+                                </div>
+                            </SheetFooter>
+                        </>
+                    )}
                 </div>
-
-                <SheetFooter className="-mx-6 bg-white p-8">
-                    <div className="w-full">
-                        <div className="mb-4 flex">
-                            <span className="flex flex-1 text-lg text-neutral-500">
-                                Итого
-                                <div className="relative -top-1 mx-2 flex-1 border-b border-dashed border-b-neutral-200" />
-                            </span>
-
-                            <span className="text-lg font-bold">{totalAmount} грн</span>
-                        </div>
-
-                        <Link href="/cart">
-                            <Button type="submit" className="h-12 w-full text-base">
-                                Оформить заказ
-                                <ArrowRight className="ml-2 w-5" />
-                            </Button>
-                        </Link>
-                    </div>
-                </SheetFooter>
             </SheetContent>
         </Sheet>
     );
--- shared/store/cart.ts
+++ shared/store/cart.ts
@@ -73,7 +73,11 @@ export const useCartStore = create<ICartState>()((set) => ({
     },
     removeCartItem: async (id: number) => {
         try {
-            set({ loading: true, error: false });
+            set((state) => ({
+                loading: true,
+                error: false,
+                items: state.items.map((item) => (item.id === id ? { ...item, disabled: true } : item)),
+            }));
             const res = await Api.cart.removeCartItem(id);
             set(getCartDetails(res));
         } catch (e) {
@@ -82,7 +86,10 @@ export const useCartStore = create<ICartState>()((set) => ({
             }
             set({ error: true });
         } finally {
-            set({ loading: false });
+            set((state) => ({
+                loading: false,
+                items: state.items.map((item) => ({ ...item, disabled: false })),
+            }));
         }
     },
 }));
