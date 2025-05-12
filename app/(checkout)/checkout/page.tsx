1c4cd50c5125883ee3c19bfaf49f86a297384e37
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2025-05-12 22:49:29 +0300

    displaying data on right sidebar of checkout page

--- app/(checkout)/checkout/page.tsx
+++ app/(checkout)/checkout/page.tsx
@@ -1,12 +1,10 @@
 'use client';
 
 import { ReactNode } from 'react';
-import { ArrowRight, Package, Percent, Truck } from 'lucide-react';
 
-import { Title, WhiteBlock, CheckoutItemDetails, CheckoutItem } from '@/shared/components/shared';
+import { Title, WhiteBlock, CheckoutItem, CheckoutSidebar } from '@/shared/components/shared';
 import { Input } from '@/shared/components/ui/input';
 import { Textarea } from '@/shared/components/ui/textarea';
-import { Button } from '@/shared/components/ui/button';
 import { getCartItemDetails } from '@/shared/lib';
 import { PizzaSize, PizzaType } from '@/shared/constants/pizza';
 import { useCart } from '@/shared/hooks';
@@ -58,47 +56,7 @@ export default function CheckoutPage({ children }: { children: ReactNode }) {
 
                 {/* Правая часть */}
                 <div className="w-[450px]">
-                    <WhiteBlock className="sticky top-4 p-6">
-                        <div className="flex flex-col gap-1">
-                            <span className="text-xl">Итого</span>
-                            <span className="text-[34px] font-extrabold">1500 грн</span>
-                        </div>
-
-                        <CheckoutItemDetails
-                            title={
-                                <div className="flex items-center">
-                                    <Package size={18} className="mr-2 text-gray-400" />
-                                    <p>Стоиомсть корзины:</p>
-                                </div>
-                            }
-                            value="500 грн"
-                        />
-
-                        <CheckoutItemDetails
-                            title={
-                                <div className="flex items-center">
-                                    <Percent size={18} className="mr-2 text-gray-400" />
-                                    <p>Налоги:</p>
-                                </div>
-                            }
-                            value="500 грн"
-                        />
-
-                        <CheckoutItemDetails
-                            title={
-                                <div className="flex items-center">
-                                    <Truck size={18} className="mr-2 text-gray-400" />
-                                    <p>Доставка:</p>
-                                </div>
-                            }
-                            value="500 грн"
-                        />
-
-                        <Button type="submit" className="mt-6 h-14 w-full rounded-2xl text-base font-bold">
-                            Перейти к оплате
-                            <ArrowRight className="ml-2 w-5" />
-                        </Button>
-                    </WhiteBlock>
+                    <CheckoutSidebar totalAmount={totalAmount} />
                 </div>
             </div>
         </>
