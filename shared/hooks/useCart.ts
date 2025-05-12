6beb5cc4f397a9751714b64d1e2069eab82d2d11
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2025-05-12 20:05:16 +0300

    realization a new hook useCart

--- shared/hooks/useCart.ts
+++ shared/hooks/useCart.ts
@@ -1,1 +1,33 @@
-export const useCart = () => {};
+import { useEffect } from 'react';
+import { useShallow } from 'zustand/react/shallow';
+import { useCartStore } from '../store';
+import { ICartStateItem } from '../lib/get-cart-details';
+
+interface IReturnValues {
+    totalAmount: number;
+    items: ICartStateItem[];
+    removeCartItem: (id: number) => Promise<void>;
+    onClickCountButton: (id: number, quantity: number, type: 'plus' | 'minus') => void;
+}
+
+export const useCart = (): IReturnValues => {
+    const [totalAmount, items, updateItemQuantity, getCartItems, removeCartItem] = useCartStore(
+        useShallow((state) => [state.totalAmount, state.items, state.updateItemQuantity, state.getCartItems, state.removeCartItem]),
+    );
+
+    const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
+        const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
+        updateItemQuantity(id, newQuantity);
+    };
+
+    useEffect(() => {
+        getCartItems();
+    }, []);
+
+    return {
+        totalAmount,
+        items,
+        removeCartItem,
+        onClickCountButton,
+    };
+};
