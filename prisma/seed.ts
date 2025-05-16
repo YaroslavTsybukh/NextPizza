aeea2687e72b22460b975d7f44be7a4a9968177c
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2025-05-16 22:38:27 +0300

    ingredient change for one pizza

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,15 +7,7 @@ const randomDecimalNumber = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min) * 10 + min * 10) / 10;
 };
 
-const generateProductItem = ({
-    productId,
-    pizzaType,
-    size,
-}: {
-    productId: number;
-    pizzaType?: 1 | 2;
-    size?: 20 | 30 | 40;
-}) => {
+const generateProductItem = ({ productId, pizzaType, size }: { productId: number; pizzaType?: 1 | 2; size?: 20 | 30 | 40 }) => {
     return {
         productId,
         price: randomDecimalNumber(190, 600),
@@ -59,8 +51,7 @@ async function up() {
     const pizza1 = await prisma.product.create({
         data: {
             name: 'Пепперони фреш',
-            imageUrl:
-                'https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp',
+            imageUrl: 'https://media.dodostatic.net/image/r:233x233/11EE7D61304FAF5A98A6958F2BB2D260.webp',
             categoryId: 1,
             ingredients: {
                 connect: _ingredients.slice(0, 5),
@@ -71,8 +62,7 @@ async function up() {
     const pizza2 = await prisma.product.create({
         data: {
             name: 'Сырная',
-            imageUrl:
-                'https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp',
+            imageUrl: 'https://media.dodostatic.net/image/r:233x233/11EE7D610CF7E265B7C72BE5AE757CA7.webp',
             categoryId: 1,
             ingredients: {
                 connect: _ingredients.slice(5, 10),
@@ -83,11 +73,10 @@ async function up() {
     const pizza3 = await prisma.product.create({
         data: {
             name: 'Чоризо фреш',
-            imageUrl:
-                'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp',
+            imageUrl: 'https://media.dodostatic.net/image/r:584x584/11EE7D61706D472F9A5D71EB94149304.webp',
             categoryId: 1,
             ingredients: {
-                connect: _ingredients.slice(10, 40),
+                connect: _ingredients.slice(9, 40),
             },
         },
     });
@@ -240,28 +229,23 @@ async function up() {
         data: [
             {
                 storyId: 1,
-                sourceUrl:
-                    'https://cdn.inappstory.ru/file/dd/yj/sx/oqx9feuljibke3mknab7ilb35t.webp?k=IgAAAAAAAAAE',
+                sourceUrl: 'https://cdn.inappstory.ru/file/dd/yj/sx/oqx9feuljibke3mknab7ilb35t.webp?k=IgAAAAAAAAAE',
             },
             {
                 storyId: 1,
-                sourceUrl:
-                    'https://cdn.inappstory.ru/file/jv/sb/fh/io7c5zarojdm7eus0trn7czdet.webp?k=IgAAAAAAAAAE',
+                sourceUrl: 'https://cdn.inappstory.ru/file/jv/sb/fh/io7c5zarojdm7eus0trn7czdet.webp?k=IgAAAAAAAAAE',
             },
             {
                 storyId: 1,
-                sourceUrl:
-                    'https://cdn.inappstory.ru/file/ts/p9/vq/zktyxdxnjqbzufonxd8ffk44cb.webp?k=IgAAAAAAAAAE',
+                sourceUrl: 'https://cdn.inappstory.ru/file/ts/p9/vq/zktyxdxnjqbzufonxd8ffk44cb.webp?k=IgAAAAAAAAAE',
             },
             {
                 storyId: 1,
-                sourceUrl:
-                    'https://cdn.inappstory.ru/file/ur/uq/le/9ufzwtpdjeekidqq04alfnxvu2.webp?k=IgAAAAAAAAAE',
+                sourceUrl: 'https://cdn.inappstory.ru/file/ur/uq/le/9ufzwtpdjeekidqq04alfnxvu2.webp?k=IgAAAAAAAAAE',
             },
             {
                 storyId: 1,
-                sourceUrl:
-                    'https://cdn.inappstory.ru/file/sy/vl/c7/uyqzmdojadcbw7o0a35ojxlcul.webp?k=IgAAAAAAAAAE',
+                sourceUrl: 'https://cdn.inappstory.ru/file/sy/vl/c7/uyqzmdojadcbw7o0a35ojxlcul.webp?k=IgAAAAAAAAAE',
             },
         ],
     });
