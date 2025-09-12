7da0fe8d62a82b240862432afa210310bd001b71
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2025-09-12 15:55:04 +0300

    adding a new library nextjs-toploader

--- shared/components/shared/providers.tsx
+++ shared/components/shared/providers.tsx
@@ -3,9 +3,11 @@
 import { FC, PropsWithChildren } from 'react';
 import { Toaster } from 'react-hot-toast';
 import { SessionProvider } from 'next-auth/react';
+import NextTopLoader from 'nextjs-toploader';
 
 export const Providers: FC<PropsWithChildren> = ({ children }) => (
     <>
+        <NextTopLoader />
         <SessionProvider>{children}</SessionProvider>
         <Toaster />
     </>
