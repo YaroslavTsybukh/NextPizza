7c7675550b3e6c6865fea6215cf3e6845184b198
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2025-09-22 17:57:53 +0300

    refactoring

--- shared/components/shared/checkout/checkout-address-form.tsx
+++ shared/components/shared/checkout/checkout-address-form.tsx
@@ -1,16 +1,12 @@
 import { FC } from 'react';
 
-import { Input, FormTextArea, WhiteBlock, AddressInput, ClearButton } from '@/shared/components';
+import { FormTextArea, WhiteBlock, AddressInput } from '@/shared/components';
 
 interface IProps {
     className?: string;
 }
 
 export const CheckoutAddressForm: FC<IProps> = ({ className }) => {
-    const onClickClear = () => console.log(1);
-    const handleOnOptionSelected = (field, option) => {
-        console.log(field, { option: option });
-    };
     return (
         <WhiteBlock title="3. Адрес доставки" className={className}>
             <div className="flex flex-col gap-5">
