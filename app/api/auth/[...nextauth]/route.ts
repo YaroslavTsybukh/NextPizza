227fe3810ceaaf5e1abe07fb4752621bb49cafed
Yaroslav Tsybukh <yaroslav.tsybukh@gmail.com>
2025-10-14 16:57:30 +0300

    deleting console.log

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,6 @@
 import NextAuth from 'next-auth';
 import { authOptions } from '@/shared/constants/auth-options';
 
-console.log('NEXTAUTH_URL exists:', !!process.env.NEXTAUTH_URL);
-console.log('NEXTAUTH_SECRET exists:', !!process.env.NEXTAUTH_SECRET);
-console.log('GOOGLE_CLIENT_ID exists:', !!process.env.GOOGLE_CLIENT_ID);
-console.log('GOOGLE_CLIENT_SECRET exists:', !!process.env.GOOGLE_CLIENT_SECRET);
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
